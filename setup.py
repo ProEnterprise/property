682d900b7875c2a153ed01fe83d0ea7d9d60fd29
Adam Dawoodjee <adam.daveed@gmail.com>
2019-04-16 19:50:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 # from pip.req import parse_requirements
 import re, ast
 
+with open('requirements.txt') as f:
+install_requires = f.read().strip().split('\n')
+
 # get version from __version__ variable in property/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open(‘requirements.txt’) as f:
-install_requires = f.read().strip().split('\n')
 
 with open('property/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
