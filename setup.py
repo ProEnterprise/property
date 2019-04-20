4080810c5801394a82b8cb24d00360d414497b19
Adam Dawoodjee <adam.daveed@gmail.com>
2019-04-20 21:07:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@
 from setuptools import setup, find_packages
 import re, ast
 
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\\n')
-
-# get version from __version__ variable in your_app/__init__.py
+# get version from __version__ variable in property/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('property/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
 
+with open('property/__init__.py', 'rb') as f:
+        version = str(ast.literal_eval(_version_re.search(
+                f.read().decode('utf-8')).group(1)))
 setup(
 	name='property',
 	version=version,
@@ -19,8 +18,7 @@ setup(
 	author='Opensource Solutions Philippines',
 	author_email='info@ossph.com',
 	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+        zip_safe=False,
+        include_package_data=True,
+        install_requires=install_requires
 )
